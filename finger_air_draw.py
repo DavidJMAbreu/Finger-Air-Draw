64b46e47eb738e97018df0a6f9c519a3c8bd2701
David Abreu <48141340+DavidJMAbreu@users.noreply.github.com>
2021-06-02 14:56:34 +0100

    Hand detection
    Resolved the inverted histogram mask problem.
    The script can now based on a preliminar skin color HSV detection and offsets detect the hand even if the brightness inc/dec.
    
    To-Do
    Filter the histogrammed result to capture the entire hand without holes.
    Clean the noise in the rest of the image e.g.: the face is considered the largest contour

--- finger_air_draw.py
+++ finger_air_draw.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy
+from numpy.lib.function_base import median
 
 
 # Carregar o video da camêra principal
@@ -9,10 +10,8 @@ webcam = cv.VideoCapture(0)
 global color_configured
 color_configured = 0
 global skinLower, skinHigher
-skinLower = numpy.array([255,255,255])
-skinHigher = numpy.array([0,255,255])
-
-
+skinLower = numpy.array([255, 255, 255])
+skinHigher = numpy.array([0, 255, 255])
 
 
 # Define a 5x5 kernel for erosion and dilation
@@ -43,55 +42,66 @@ def draw_hand_circle(drawing_frame):
 
     # Desenhar os circulos na imagem
     for i in range(number_circle):
-        cv.circle(drawing_frame, (circle_x[i], circle_y[i]), 10, (0, 0, 255), 1)
+        cv.circle(drawing_frame,
+                  (circle_x[i], circle_y[i]), 10, (0, 0, 255), 1)
 
     # Text de indicação
     cv.putText(drawing_frame, "Position hand and press 's'", (200, 30),
                cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 0, 0), 1)
 
 
-
 # Função para calibrar os vetores de cor a procurar na imagem (em HSV)
 def color_calibration(calibration_frame):
-    global skinHigher,skinLower,color_configured
-
-    cv.imshow("Calibration",calibration_frame)
+    global skinHigher, skinLower, color_configured
 
     # Passar a calibration_frame para HSV para funcionarmos em relação de cor e intensidade
     calibration_frame = cv.cvtColor(calibration_frame, cv.COLOR_BGR2HSV)
 
-    current = numpy.array([],
-                         dtype=numpy.uint8)
+    l_v = 255
+    l_h = 255
+    l_s = 255
+    g_v = 0
+    g_h = 0
+    g_s = 0
 
     for i in range(number_circle):
-    
+
         # Obter o valor médio numa vizinha de 9x9 pixeis
-        h=0
-        s=0
-        v=0
+        v = 0
+        s = 0
+        h = 0
         for j in range(9):
             for k in range(9):
-                h += calibration_frame[(circle_y[i]-4+j),(circle_x[i]-4+k)][0]
-                s += calibration_frame[(circle_y[i]-4+j),(circle_x[i]-4+k)][1]
-                v += calibration_frame[(circle_y[i]-4+j),(circle_x[i]-4+k)][2]
-            
-        h /= 81
-        s /= 81
-        v /= 81
+                print(calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)])
+                h += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][0]
+                s += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][1]
+                v += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][2]
 
-        current = [round(h),round(s),round(v)]
-        if (current[0]>skinHigher[0]):
-            skinHigher = numpy.array([current[0]+10,current[1]+10,current[2]+20])
-        
-        if (current[0]<skinLower[0]):
-            skinLower = numpy.array([current[0]-10,current[1]+10,current[2]-20])
-    
+        v /= 81
+        s /= 81
+        h /= 81
+
+        if(v > g_v and s > g_s and h > g_h):
+            g_v = round(v)
+            g_s = round(s)
+            g_h = round(h)
+
+        if(v < l_v and s < l_s and h < l_h):
+            l_v = round(v)
+            l_s = round(s)
+            l_h = round(h)
+
+    skinHigher = numpy.array([round(g_h)+50, round(g_s)+50, 255])
+    skinLower = numpy.array([round(l_h)-10, round(l_s)-10, round(l_v)-20])
+    print(skinLower)
+    print("\n\n", skinHigher)
 
     color_configured = 1
-        
-    
+
+
 def doNothing(x):
     pass
+
 
 # Create a window
 cv.namedWindow('Mask')
@@ -107,32 +117,42 @@ while True:
 
     # Inverter o video
     frame = cv.flip(frame, 1)
+
     frame_circle = frame
 
     if color_configured == 1:
-        frame = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
 
-        mask = cv.inRange(frame,numpy.array([100,60,60]),numpy.array([140, 255, 255]))
-        
-        edges = cv.Canny(mask,50,150)
-        
+        mask = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        mask = cv.inRange(mask, skinLower, skinHigher)
+
+        # mask = cv.morphologyEx(mask,cv.MORPH_OPEN,numpy.ones((3,3),dtype=numpy.uint8),iterations=2)
+        mask = cv.dilate(mask, numpy.ones(
+            (3, 3), dtype=numpy.uint8), iterations=2)
+
+        medianFilter = cv.medianBlur(mask, 11)
+        gaussian = cv.GaussianBlur(mask, (5, 5), 0)
+
+        edges = cv.Canny(medianFilter, 100, 150)
+
+        contours, hierarchy = cv.findContours(
+            edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        if contours != []:
+            largest = max(contours, key=cv.contourArea)
+            cv.drawContours(frame, largest, 0, (80, 255, 255), 3)
 
         cv.imshow("Mask", mask)
         cv.imshow("edges", edges)
-    
+        cv.imshow("median", medianFilter)
+
     # Se durante o periodo de configuração a tecla Ctrl for pressionada
-    if color_configured==0 and cv.waitKey(10) == 115:
+    if color_configured == 0 and cv.waitKey(10) == 115:
         # Fazer o histograma das cores da mão com base nos circulos definidos
         color_calibration(frame)
 
-    if color_configured==0:
+    if color_configured == 0:
         draw_hand_circle(frame_circle)
-        
-
-    
-
-        
-
 
     cv.imshow("Video", frame_circle)
 
--- test.py
+++ test.py
@@ -1,44 +1,60 @@
-import numpy as np
+# Required moduls
 import cv2
-cap=cv2.VideoCapture(0)
-_,old_frame=cap.read()
-mask1 = np.zeros_like(old_frame)
-counter=0
-while(1) :
-    check, frame=cap.read()
+import numpy
+
+# Constants for finding range of skin color in YCrCb
+min_YCrCb = numpy.array([0,133,77],numpy.uint8)
+max_YCrCb = numpy.array([255,173,127],numpy.uint8)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+gameflag = False
+
+# Create a window to display the camera feed
+cv2.namedWindow('Camera Output')
+
+# Get pointer to video frames from primary device
+videoFrame = cv2.VideoCapture(0)
+
+while True: # any key pressed has a value >= 0
+
+    # Grab video frame, decode it and return next video frame
+    readSucsess, sourceImage = videoFrame.read()
+    sourceImage = cv2.flip(sourceImage, 1)
+
+    # Convert image to YCrCb
+    imageYCrCb = cv2.cvtColor(sourceImage,cv2.COLOR_BGR2YCR_CB)
+
+    # Find region with skin tone in YCrCb image
+    skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
     
-    hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  
-    blur = cv2.GaussianBlur(hsv, (11,11),0)
-    
-    l_orange=np.array([10,180,180])
-    h_orange=np.array([30,255,255])
-    
-    mask = cv2.inRange(hsv, l_orange , h_orange)
-    mblur = cv2.GaussianBlur(mask,(15,15),0)
-    
-    ret,thresh = cv2.threshold(mblur,127,255,0)
-    image, contours, heirarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    img = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
-    if( contours != []):
-        cnt = contours[0]
-        M = cv2.moments(cnt)
-        if M['m00']!=0:
-            cx = int (M['m10']/M['m00'])
-            cy = int (M['m01']/M['m00'])
-            if (counter==0) :
-                cx1=cx
-                cy1=cy
-            if(counter==0):
-                counter += 1
-            mask1 = cv2.line(mask1, (cx,cy),(cx1,cy1),(0.0,255),2)
-            frame = cv2.circle(frame,(cx,cy),8,(0,0,255),-1)
-            cx1=cx
-            cy1=cy
-        img=cv2.add(frame,mask1)
-        cv2.imshow("ori",img)
-        cv2.imshow("mask",mask1)
-        k=cv2.waitKey(1)
-        if k==ord('q'):
-            break
-cap.release()
-cv2.destroyAllWindows()
+    skin = cv2.bitwise_and(sourceImage, sourceImage, mask = skinRegion)
+
+    # Do contour detection on skin region
+    contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.rectangle(skin,(0,300),(640,200),(0,0,255),3)
+    if len(contours) != 0:
+        contour = max(contours, key = cv2.contourArea)
+        M = cv2.moments(contour)
+        if(M["m00"] != 0):
+            cx = int(M["m10"]/M["m00"])
+            cy = int(M["m01"]/M["m00"])
+            area = M["m00"]
+            if cy < 200:
+                cv2.circle(skin, (cx, cy), 7, (0, 255, 0), -1)
+            elif cy > 300:
+                cv2.circle(skin, (cx, cy), 7, (255, 0, 0), -1)
+            else:
+                cv2.circle(skin, (cx, cy), 7, (255, 255, 255), -1)
+                gameflag = True
+                
+
+    # Display the source image
+    cv2.imshow('Camera Output',skin)
+
+    # Check for user input to close program
+    if cv2.waitKey(5) == 27: # wait 5 millisecond in each iteration of while loop
+        break 
+
+# Close window and camera after exiting the while loop
+cv2.destroyWindow('Camera Output')
+videoFrame.release()
