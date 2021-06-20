d27654fbf4ab2dc90a2dc21e4877411002490ba5
David Abreu <48141340+DavidJMAbreu@users.noreply.github.com>
2021-06-20 12:49:14 +0100

    v0.8
    Completed, only left to show draw next to image captured

--- finger_air_draw.py
+++ finger_air_draw.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy
-from numpy.lib.function_base import median
+from collections import deque
 
 
 # Carregar o video da camêra principal
@@ -14,8 +14,41 @@ skinLower = numpy.array([255, 255, 255])
 skinHigher = numpy.array([0, 255, 255])
 
 
-# Define a 5x5 kernel for erosion and dilation
-kernel = numpy.ones((5, 5), numpy.uint8)
+# Variáveis para armazenar os pixeis desenhados
+pontos_azul = [deque(maxlen=512)]
+pontos_verde = [deque(maxlen=512)]
+pontos_vermelho = [deque(maxlen=512)]
+pontos_preto = [deque(maxlen=512)]
+pontos_amarelo = [deque(maxlen=512)]
+# Indices para as cores
+blindex = 0
+bindex = 0
+gindex = 0
+rindex = 0
+yindex = 0
+# Vetor com as cores a utilizar
+# preto - azul - verde - vermelho - amarelo
+cores = [(0,0,0),(255,0,0),(0,255,0),(0,0,255),(0,255,255)]
+cor_atual = 0
+
+#Tela branca
+tela = numpy.zeros((480,320,3))+255
+tela = cv.rectangle(tela, (10,1), (52,50), (0,0,0), 2)
+tela = cv.rectangle(tela, (62,1), (104,50), cores[0], -1)
+tela = cv.rectangle(tela, (114,1), (156,50), cores[1], -1)
+tela = cv.rectangle(tela, (166,1), (208,50), cores[2], -1)
+tela = cv.rectangle(tela, (218,1), (260,50), cores[3], -1)
+tela = cv.rectangle(tela, (270,1), (312,50), cores[4], -1)
+cv.putText(tela, "CL", (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv.LINE_AA)
+cv.putText(tela, "0", (25, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv.LINE_AA)
+cv.putText(tela, "1", (77, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+cv.putText(tela, "2", (129, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+cv.putText(tela, "3", (181, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+cv.putText(tela, "4", (233, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv.LINE_AA)
+cv.putText(tela, "5", (285, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv.LINE_AA)
+
+
+
 
 
 # Função para desenhar os circulos de demonstração da área de aquisição de cor para o histograma
@@ -72,7 +105,6 @@ def color_calibration(calibration_frame):
         h = 0
         for j in range(9):
             for k in range(9):
-                print(calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)])
                 h += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][0]
                 s += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][1]
                 v += calibration_frame[(circle_y[i]-4+j), (circle_x[i]-4+k)][2]
@@ -93,8 +125,6 @@ def color_calibration(calibration_frame):
 
     skinHigher = numpy.array([round(g_h)+50, round(g_s)+100, 255])
     skinLower = numpy.array([round(l_h)-20, round(l_s)-30, round(l_v)-80])
-    print(skinLower)
-    print("\n\n", skinHigher)
 
     color_configured = 1
 
@@ -102,9 +132,57 @@ def color_calibration(calibration_frame):
 def doNothing(x):
     pass
 
+def getPoint(contour):
+    #Obter as linhas para descobrir a mais alta
+    lines = []
+    points = []
+    index = 0
+    for c in contour:
+        lines.append(c[0][1])
+    
+    highest = min(lines)
+    while(index<len(lines)):
+        try:
+            new_find = lines.index(highest,index)
+            points.append(new_find)
+            index = new_find+1
+        except ValueError as e:
+            break        
 
-# Create a window
-cv.namedWindow('Mask')
+    #Obter as colunas e fazer a média entre todos
+    col = 0
+    for point in points:
+        col += contour[point][0][0]
+    col = round(col/len(points))
+
+    return [highest,col]
+
+def savePoint(point):
+    if cor_atual == 0:
+        pontos_preto[blindex].appendleft(point)
+    elif cor_atual == 1:
+        pontos_azul[bindex].appendleft(point)
+    elif cor_atual == 2:
+        pontos_verde[gindex].appendleft(point)
+    elif cor_atual == 3:
+        pontos_vermelho[rindex].appendleft(point)
+    elif cor_atual == 4:
+        pontos_amarelo[yindex].appendleft(point)
+
+def draw(tela):
+    points = [pontos_preto,pontos_azul,pontos_verde,pontos_vermelho,pontos_amarelo]
+    for color in range(len(points)):
+        for deque in range(len(points[color])):
+            for point in range(len(points[color][deque])):
+                if points[color][deque][point - 1] is None or points[color][deque][point] is None:
+                    continue
+                cv.line(tela, points[color][deque][point - 1], points[color][deque][point], cores[cor_atual], 2)
+                print(points[color][deque][point])
+            
+
+
+    
+
 
 if not webcam.isOpened:
     print('--(!)Error opening video capture')
@@ -134,12 +212,12 @@ while True:
         medianFilter = cv.medianBlur(mask, 11)
         gaussian = cv.GaussianBlur(mask, (5, 5), 0)
 
-
+        cv.imshow("media",medianFilter)
         
 
         contours, hierarchy = cv.findContours(medianFilter, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
          
-        cv.drawContours(medianFilter, contours, -1, 255, -1)
+        
         if contours != []:
             # Detetar qual a maior àrea
             largest = 0
@@ -150,48 +228,75 @@ while True:
                     max_area = area
                     largest = contour
 
-            centroid =  centroid(largest)
-            #approx the contour a little
-            epsilon = 0.0005*cv.arcLength(largest,True)
-            approx= cv.approxPolyDP(largest,epsilon,True)
+            column,line = getPoint(largest)
 
-            hull= cv.convexHull(largest,returnPoints=True)
-             #define area of hull and area of hand
-            areahull = cv.contourArea(hull)
-            areacnt = cv.contourArea(largest)
+            cv.circle(frame[0:480,round(col/2):col,0:3],(line,column+15),2,cores[cor_atual],-1)
+            cv.circle(tela,(line,column+15),2,cores[cor_atual],-1)
 
-        #find the percentage of area not covered by hand in convex hull
-            arearatio=((areahull-areacnt)/areacnt)*100
-
-        #find the defects in convex hull with respect to hand
-            hull = cv.convexHull(approx, returnPoints=False)
-            defects = cv.convexityDefects(approx, hull)
-
-            hull= cv.convexHull(largest,returnPoints=False)
-            draw_hull= cv.convexHull(largest)
-            cv.drawContours(frame[0:480,round(col/2):col,0:3], contours,0,(0,255,0),2)
-            #Obter o ponto mais longe do centro
-            for defect in defects:
-                print('defeito ',largest[defect[0][2]][0])
-                cv.circle(frame[0:480,round(col/2):col,0:3],largest[defect[0][2]][0],10,(255,255,255),3)
+            savePoint([line,column])
                 
                     
-                    
-        
-            
-
-        cv.imshow("Mask", mask)
-        cv.imshow("median", medianFilter)
 
     # Se durante o periodo de configuração a tecla Ctrl for pressionada
-    if color_configured == 0 and cv.waitKey(10) == 115:
+    if color_configured == 0 and cv.waitKey(1) == 115:
         # Fazer o histograma das cores da mão com base nos circulos definidos
         color_calibration(frame)
+
+    if color_configured == 1:
+        if cv.waitKey(1) == 114:
+            color_configured = 0
+        else:
+            key = cv.waitKey(1)
+            if key != -1:
+                if key == 48:
+                    pontos_preto = [deque(maxlen=512)]
+                    pontos_azul = [deque(maxlen=512)]
+                    pontos_verde = [deque(maxlen=512)]
+                    pontos_vermelho = [deque(maxlen=512)]
+                    pontos_amarela = [deque(maxlen=512)]
+
+                    blindex = 0
+                    bindex = 0
+                    gindex = 0
+                    rindex = 0
+                    yindex = 0
+                    tela = numpy.zeros((480,320,3))+255
+                    tela = cv.rectangle(tela, (10,1), (52,50), (0,0,0), 2)
+                    tela = cv.rectangle(tela, (62,1), (104,50), cores[0], -1)
+                    tela = cv.rectangle(tela, (114,1), (156,50), cores[1], -1)
+                    tela = cv.rectangle(tela, (166,1), (208,50), cores[2], -1)
+                    tela = cv.rectangle(tela, (218,1), (260,50), cores[3], -1)
+                    tela = cv.rectangle(tela, (270,1), (312,50), cores[4], -1)
+                    cv.putText(tela, "CL", (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv.LINE_AA)
+                    cv.putText(tela, "0", (25, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv.LINE_AA)
+                    cv.putText(tela, "1", (77, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+                    cv.putText(tela, "2", (129, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+                    cv.putText(tela, "3", (181, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv.LINE_AA)
+                    cv.putText(tela, "4", (233, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv.LINE_AA)
+                    cv.putText(tela, "5", (285, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv.LINE_AA)
+                else:
+                    if key == 49:
+                        cor_atual = 0
+                    else:
+                        if key == 50:
+                            cor_atual = 1
+                        else:
+                            if key == 51:
+                                cor_atual = 2
+                            else:
+                                if key == 52:
+                                    cor_atual = 3
+                                else:
+                                    if key == 53:
+                                        cor_atual = 4
+            
 
     if color_configured == 0:
         draw_hand_circle(frame_circle)
 
     cv.imshow("Video", frame_circle)
+    draw(tela)
+    cv.imshow("Tela",tela)
 
-    if cv.waitKey(10) == 27:
+    if cv.waitKey(1) == 27:
         break
